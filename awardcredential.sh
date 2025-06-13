14d97e3c66d241d71206fc4b44503322362a2923
neeraj3235 <neerajsalehittal3235@gmail.com>
2025-06-13 10:58:45 +0530

    update

--- awardcredential.sh
+++ awardcredential.sh
@@ -1,4 +1,4 @@
-curl --request POST \
+curl -v --request POST \
     --url https://preprod.certifyme.online/api/v2/credential \
     --header 'Authorization: eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjo1NTk5LCJpYXQiOjE3NDk2MjUyOTZ9.-4bffndj_vbKfurooPLEdqlTj1xuOnYcDPFoOGT9iVk' \
     --header 'accept: application/json' \
@@ -7,6 +7,6 @@ curl --request POST \
 {
   "template_ID": "25012",
   "name": "karan ",
-  "email": "neeraj@certifyme.cc"
-}
+  "email": "neerajsalehittal3235@gmail.com"
+  }
 '
