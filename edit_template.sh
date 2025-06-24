cdbf23eaf7d20914f7e61843bab393f052d0d0e2
neeraj3235 <neerajsalehittal3235@gmail.com>
2025-06-24 12:37:45 +0530

    update

--- edit_template.sh
+++ edit_template.sh
@@ -1,10 +1,10 @@
 curl --request PUT \
-  --url https://preprod.certifyme.online/api/advanced/v2/template/edit/25013 \
+  --url https://preprod.certifyme.online/api/advanced/v2/template/edit/25113 \
   --header 'Authorization: eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjo1NTk5LCJpYXQiOjE3NDk2MjUyOTZ9.-4bffndj_vbKfurooPLEdqlTj1xuOnYcDPFoOGT9iVk' \
   --header 'accept: application/json' \
   --header 'content-type: application/json' \
   --data '{
-    "event": "sdxzzc",
+    "event": "Neeraj Created the template for the business purpose",
     "institutionName": "XcZXCZCZ",
     "text": "ZXCZXc",
     "skills": "ZXcxzcZC",
